234d982c86f06c54b7d5fb82541899257382c659
Iamneo <techadmin@iamneo.ai>
2023-11-17 11:21:38 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,24 +1,24 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'angularapp';
-  employeeName:string='John Doe';
-
-  isPresent:boolean=true;
+  title = 'angularapp';
+  employeeName:'John Doe';
+ 
+  isPresent=true;
   onToggleAttendance(){
     this.isPresent=!this.isPresent;
-    
+   
   }
   imageUrl='path/to/employee-image.jpg';
   colSpan=2;
-
+ 
   isSpecial=true;
-
-  textColor='red';
+ 
+  textColor='blue';
   employeeStatus='';
 }
